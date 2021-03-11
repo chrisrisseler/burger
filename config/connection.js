93fca46f89126aced8c8bd3d1e13ae1ec1161d2f
Chris Risseler <chrisrisseler43@gmail.com>
2021-03-10 21:56:53 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    // NOTE: Be sure to add your MySQL password here!
-    password: '',
-    database: 'burgers_db',
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        // NOTE: Be sure to add your MySQL password here!
+        password: '12345',
+        database: 'burgers_db',
+    });
+}
+
 
 // Make connection.
 connection.connect((err) => {
